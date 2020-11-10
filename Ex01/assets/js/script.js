a99e50bfdc94b0150f904a1adda9bcbdc5fe4362
kevinmilet <kevin.milet@gmail.com>
2020-11-10 12:19:54 +0100

    exo 1 ok

---
+++ Ex01/assets/js/script.js
@@ -0,0 +1,20 @@
+const buttonElement = document.getElementById('btn');
+
+// on crée une fonction qui récupére les valeurs des inputs et qui appelle la fonction multiply pour afficher le résultat
+buttonElement.onclick = function (){
+    // on récupére les valeurs des imputs et on les converti en entier
+    let number1 = parseInt(document.getElementById('number1').value);
+    let number2 = parseInt(document.getElementById('number2').value);
+
+    // on appelle la fonction multiply et on stocke le résultat dans une variable
+    let result = multiply(number1, number2);
+
+    // on affiche le résultat
+    document.getElementById('result').innerHTML ='Le résultat de la multiplication est: ' + result;
+
+}
+
+// fonction qui multiplie les deux nombres et renvoie le résultat
+function multiply(number1, number2) {
+    return number1 * number2;
+}
