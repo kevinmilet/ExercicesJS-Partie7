0a6b8f8e66e18a89baefaef30feeb9d9cf16369b
kevinmilet <kevin.milet@gmail.com>
2020-11-12 10:17:19 +0100

    exo 7 up ok

---
+++ Ex06/assets/js/script.js
@@ -0,0 +1,9 @@
+// (name) => {
+//     `Bonjour, ${name} ! Comment vas-tu ?`;
+// }
+
+function bonjour(name) {
+    return `Bonjour, ${name} ! Comment vas-tu ?`;
+}
+
+console.log(bonjour('kevin'));
