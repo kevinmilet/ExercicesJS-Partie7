2914c9cf91a7c36fb5e0c076fe2ff75977f5607c
kevinmilet <kevin.milet@gmail.com>
2020-11-12 09:33:19 +0100

    up exo 3 ok

---
+++ Ex03/assets/js/script.js
@@ -0,0 +1,14 @@
+// on récupére l'id du bouton et on le stock dans une constante pour l'appeller ensuite
+const buttonElement = document.querySelector('#btn');
+
+// fonction fléchée qui récupére les nombres dans deux variables, les multiplies et affiche le résultat
+let multiply = () => {
+    let number1 = parseInt(document.querySelector('#number1').value);
+    let number2 = parseInt(document.querySelector('#number2').value);
+    let result = number1 * number2;
+    document.querySelector('#result').innerHTML ='Le résultat de la multiplication est: ' + result;
+};
+
+// Event listener au click sur le bouton pour appeler la fonction
+buttonElement.addEventListener('click', multiply);
+
