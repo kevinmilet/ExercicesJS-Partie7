07d2bee3e9731d331ef8668fce71ce49084374d0
kevinmilet <kevin.milet@gmail.com>
2020-11-12 09:58:17 +0100

    exo 4 ajout commentaires

--- Ex04/assets/js/script.js
+++ Ex04/assets/js/script.js
@@ -1,5 +1,7 @@
+// on récupére la balise img dans une contanste pour l'utiliser dans la fonction et dans l'event listener
 const pict = document.querySelector('img');
 
+// fonction fléchée qui scale l'image au click
 let scaleImg = () => {
     if (pict.style.width == '50%') {
         pict.style.width = '100%';
@@ -8,4 +10,5 @@ let scaleImg = () => {
     };
 }
 
+// listener qui appele la fonction scaleImg
 pict.addEventListener('click', scaleImg);
